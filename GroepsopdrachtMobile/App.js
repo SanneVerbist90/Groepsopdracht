c94d80e8d0a518871037fa990d35a2662fc34957
SanneVerbist90 <60688549+SanneVerbist90@users.noreply.github.com>
2020-12-03 17:58:44 +0100

    Added map, basic nav on main page

--- GroepsopdrachtMobile/App.js
+++ GroepsopdrachtMobile/App.js
@@ -1,26 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackView } from '@react-navigation/stack';
 import { NavigationContainer, TabActions } from '@react-navigation/native';
-
+import MapView from 'react-native-maps';
 import { StyleSheet, Text, View } from 'react-native';
 import MapScreen from './components/MapScreen';
-import ListScreen from './components/ListScreen'
+import ListScreen from './components/ListScreen';
 
-//const Tab = createBottomTabNavigator();
-//const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 //icoontjes toevoegen in nav
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Map" component={MapScreen} />
-        <Tab.Screen name="List" component={ListScreen} />
+        <Tab.Screen name="Map" component={MapViewScreenStack} />
+        <Tab.Screen name="Lijst" component={ListScreenStack} />
       </Tab.Navigator>
     </NavigationContainer>
   );
+}
+
+export const MapViewScreenStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name ="MapScreen" component={MapScreen} options ={{ title: "Map"}}/>
+    </Stack.Navigator>
+  )
+}
+
+export const ListScreenStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name ="ListScreen" component={ListScreen} options ={{ title: "List"}}/>
+    </Stack.Navigator>
+  )
 }
 
 const styles = StyleSheet.create({
--- GroepsopdrachtMobile/components/MapScreen.js
+++ GroepsopdrachtMobile/components/MapScreen.js
@@ -2,24 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
-import {Marker} from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 
 
 export default MapScreen = () => {
   return (
     <View style={styles.container}>
-      <MapView style={ styles.mapStyle} mapType={"standard"}
+      <MapView style={styles.mapStyle} mapType={"standard"}
         region={{
-          latitude: 51.212652,
-          longitude: 4.413971,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-        >
-
-        </MapView>
-      
-      <StatusBar style="auto" />
+          latitude: 51.231107,
+          longitude: 4.415127,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}>
+      </MapView>
     </View>
   );
 }
@@ -28,10 +24,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
   mapStyle: {
-    flex: 1
+    flex: 1,
   }
 });

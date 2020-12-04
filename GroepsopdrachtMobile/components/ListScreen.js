959e91838bd2bbfcfa7278b2163a66e5649aa2fa
emma <s079839@ap.be>
2020-12-04 18:10:52 +0100

    async api fetch werkt

--- GroepsopdrachtMobile/components/ListScreen.js
+++ GroepsopdrachtMobile/components/ListScreen.js
@@ -1,19 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+
+
+
 
 export default ListScreen = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.jsonbin.io/b/5fca6286516f9d1270281279')
+      .then((response) => response.json())
+      .then((json) => setData(json.features))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Dag Emma, hier komt de lijst!</Text>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={( item , index) => index.toString()}
+          renderItem={({ item }) => (
+            <Text>{item.attributes.NAAM}</Text>
+          )}
+        />
+      )}
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+
+
+
+
+// export default ListScreen = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text> [getInformationFromApiAsync] </Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',

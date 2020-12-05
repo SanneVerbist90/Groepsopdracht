ca09d20b2a1a4044133887f786cb2550408c4992
SanneVerbist90 <60688549+SanneVerbist90@users.noreply.github.com>
2020-12-05 13:01:11 +0100

    Added markers,

--- GroepsopdrachtMobile/components/MapScreen.js
+++ GroepsopdrachtMobile/components/MapScreen.js
@@ -4,23 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 
-//Fetch API
-const ApiUrl = "https://opendata.arcgis.com/datasets/99c7168df28142958cbfec31cd633d56_289.geojson";
-function getData() {
-  return fetch(ApiUrl)
-  .then(data => data.json())
-}
-
 export default MapScreen = () => {
-  const [data, setData] = useState();
-  useEffect(()=> {
-    getData()
-    .then(items => {
-      setData(items)
-    })
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://api.jsonbin.io/b/5fca6286516f9d1270281279')
+      .then((response) => response.json())
+      .then((json) => setData(json.features))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
   }, []);
-  console.log(data);
 
+ // console.log(data.map((marker, index)=>(marker.geometry.y)));
   return (
     <View style={styles.container}>
       <MapView style={styles.mapStyle} mapType={"standard"}
@@ -30,6 +25,8 @@ export default MapScreen = () => {
           latitudeDelta: 0.1,
           longitudeDelta: 0.1,
         }}>
+          {data.map((marker, index) => (<Marker key ={index} coordinate={{latitude: marker.geometry.y, longitude: marker.geometry.x}}/>))}
+         
       </MapView>
     </View>
   );
